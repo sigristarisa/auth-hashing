7b5e72933b8c36c40655660efda392a95f45e21e
sigristarisa <arisasigrist.ch@gmail.com>
2022-06-15 13:45:22 +0200

    finished the second requirement

--- src/routers/sessions.js
+++ src/routers/sessions.js
@@ -1,26 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const prisma = require('../utils/prisma.js')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const prisma = require("../utils/prisma.js");
 
-router.post('/', async (req, res) => {
-    const { username, password } = req.body;
+router.post("/", async (req, res) => {
+  const { username, password } = req.body;
+  const foundUser = await prisma.user.findFirst({
+    where: {
+      username,
+    },
+  });
+  const passwordIsValid = await bcrypt.compare(password, foundUser.password);
+  console.log("passwordIsValid", passwordIsValid);
 
-    const foundUser = await prisma.user.findFirst({
-        where: {
-            username
-        }
-    });
+  if (!foundUser || !passwordIsValid) {
+    return res.status(401).json({ error: "Invalid username or password." });
+  }
 
-    if (!foundUser || password !== foundUser.password) {
-        return res.status(401).json({ error: 'Invalid username or password.' });
-    }
+  const token = jwt.sign({ username }, process.env.JWT_SECRET);
 
-    const token = jwt.sign({ username }, process.env.JWT_SECRET);
-
-    res.json({ data: token });
+  res.json({ data: token });
 });
 
 module.exports = router;
